ebd36d1b2f8989b3e2b3d31c8c24c61f29d1a4f8
stm33 <satuma33@icloud.com>
2023-02-13 23:56:21 +0900

    [add]refs #34-binary-seaceh-tree

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -4,14 +4,14 @@ from typing import List
 def bubble_sort(nums: List[int]) -> List[int]:
     len_nums = len(nums)
     for i in range(len_nums):
-        for j in range(len_nums - 1 - i):
+        for j in range(len_nums-1 - i):
             if nums[j] > nums[j+1]:
                 nums[j], nums[j+1] = nums[j+1], nums[j]
 
     return nums
 
 
-if __name__ == "__main__":
-    nums = [2, 5, 1, 8, 7, 3]
-    output = bubble_sort(nums)
-    print(output)
+if __name__ == '__main__':
+    import random
+    nums = [random.randint(0, 50) for i in range(5)]
+    print(bubble_sort(nums))
