0876901a939e96cd0048c4d4f90f31261f55b63e
stm33 <satuma33@icloud.com>
2023-03-07 18:45:32 +0900

    [add]refs #57-memoize

---
+++ quiz/min_count_remove.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from typing import List
+
+
+def min_count_remove(x: List[int], y: List[int]) -> None:
+    counter_x = Counter(x)
+    counter_y = Counter(y)
+
+    for key_x, value_x in counter_x.items():
+        value_y = counter_y.get(key_x)
+        if value_y:
+            if value_x < value_y:
+                x[:] = [i for i in x if i != key_x]
+            elif value_x > value_y:
+                y[:] = [i for i in y if i != key_x]
+
+
+if __name__ == '__main__':
+    x = [1, 2, 3, 4, 4, 5, 5, 8, 10]
+    y = [4, 5, 5, 5, 6, 7, 8, 8, 10]
+    min_count_remove(x, y)
+    print(x, y)
